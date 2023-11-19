84cfed44fe58f63a9512b79dff97e3290af3d7b9
Ar-sls <151406582+Ar-sls@users.noreply.github.com>
2023-11-19 13:28:57 +0200

    Add files via upload

---
+++ Back End/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+namespace WebApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+        public static IHostBuilder CreateWebHostBuilder(string[] args) =>
+          Host.CreateDefaultBuilder(args)
+              .ConfigureWebHostDefaults(webBuilder =>
+              {
+                 
+                  webBuilder.UseStartup<Startup>();
+              });
+       
+    }
+}
---
+++ Back End/Startup.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WebApi.Helpers;
+using WebApi.Services;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
+using Microsoft.Extensions.Hosting;
+
+namespace WebApi
+{
+    public class Startup
+    {      
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;            
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            
+            var appConnectionstr = Configuration.GetSection("Connectionstr");
+            services.Configure<Connection>(appConnectionstr);
+            var con1 = appConnectionstr.Get<Connection>();
+
+            services.AddCors(c =>
+            {
+                c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
+            });
+
+          
+            var appSettingsSection = Configuration.GetSection("AppSettings");
+            services.Configure<AppSettings>(appSettingsSection);
+
+            services.AddControllers();          
+            services.AddScoped<IBlogsService, BlogsService>();           
+            services.AddSwaggerGen();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+                app.UseDeveloperExceptionPage();
+            }
+           
+            // Configure the HTTP request pipeline.           
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+            app.UseEndpoints(x => x.MapControllers());
+        }
+    }
+}
